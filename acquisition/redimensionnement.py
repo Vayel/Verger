e25c409e4e6360d7138aa2082d6f18ac7f79d5d7
vincent <vincent.lefoulon@free.fr>
2014-05-04 19:40:00 +0200

    Script de redimensionnement

--- acquisition/redimensionnement.py
+++ acquisition/redimensionnement.py
@@ -1,12 +1,78 @@
 # -*- coding: utf-8 -*-
 
+import os
 import sys
+
 # Path from gimp.__file__ in GIMP's Python-Fu console
 sys.path.append("/usr/lib/gimp/2.0/python")
+
 import gimp
 
-def main():
-	help(gimp)
+SIZE = 800
+allowedExts = ['jpg', 'jpeg', 'png']
+
+
+def parsePath(path):
+	name, ext = os.path.splitext(path)
+	ext = ext[1:].lower() # Remove "."
+	
+	return name, ext
+
+
+def checkPath(path):
+	if not(os.path.isfile(path)):
+		return False
+		
+	name, ext = parsePath(path)
+	
+	return (ext in allowedExts)
+
+
+def createIm(path):
+	name, ext = parsePath(path)
+	
+	if ext == 'jpg' or ext == 'jpeg':
+		return gimp.pdb.file_jpeg_load(path, path)
+	elif ext == 'png':
+		return gimp.pdb.file_png_load(path, path)
+
+
+def getImDims(im, size):
+	width = gimp.pdb.gimp_image_width(im)
+	height = gimp.pdb.gimp_image_height(im)
+	m = max(width, height)
+	
+	if m <= size:
+		return width, height
+	else:
+		factor = float(m)/size
+		return (float(width)/factor), (float(height)/factor)
+	
+
+def main(paths):
+	for path in paths:
+		# Check files
+		if not(checkPath(path)):
+			print 'Error with {}'.format(path) 
+			return
+		
+		# Create image
+		im = createIm(path)
+		
+		# Resize it
+		width, height = getImDims(im, SIZE)
+		gimp.pdb.gimp_image_scale(im, width, height)
+		
+		# Save it
+		# TODO: 
+		"""
+		gimp.pdb.file_jpeg_save(im, drawable, filename, raw_filename, quality, smoothing, optimize, progressive, comment, subsmp, baseline, restart, dct)
+		gimp.pdb.file_png_save(im, drawable, filename, raw_filename, interlace, compression, bkgd, gama, offs, phys, time)
+		"""
+			
 	
 if __name__ == '__main__':
-	main()
+	if len(sys.argv) > 1:
+		main(sys.argv[1:])
+	else:
+		print 'No image to be resized.'
